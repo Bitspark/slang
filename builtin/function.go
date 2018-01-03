9a8acc5d76afc7dd9446368523217173c2f15c8f
Julian Matschinske <julian.matschinske@bitspark.de>
2018-01-03 12:22:11 +0100

    Add more stubs
    * Manager stubs
    * Empty function operator

---
+++ builtin/function.go
@@ -0,0 +1,10 @@
+package builtin
+
+import (
+	"slang"
+	"errors"
+)
+
+func functionCreator(properties map[string]interface{}) (*slang.Operator, error) {
+	return nil, errors.New("not implemented")
+}
--- builtin/manager.go
+++ builtin/manager.go
@@ -1,9 +1,36 @@
 package builtin
 
-type manager struct {
+import (
+	"slang"
+	"errors"
+)
 
+type CreatorFunc func(properties map[string]interface{}) (*slang.Operator, error)
+
+type Manager struct {
+	creators map[string]CreatorFunc
 }
 
-func Manager() *manager {
-	return nil
+var m = Manager{}
+
+func init() {
+	m.Register("function", functionCreator)
+}
+
+func M() Manager {
+	return m
+}
+
+func (m Manager) MakeOperator(name string, properties map[string]interface{}) (*slang.Operator, error) {
+	creator, ok := m.creators[name]
+
+	if !ok {
+		return nil, errors.New("unknown builtin operator")
+	}
+
+	return creator(properties)
+}
+
+func (m Manager) Register(name string, creator CreatorFunc) {
+	m.creators[name] = creator
 }

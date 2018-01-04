489a8cb5741f234d7aa254c9493dccc23dc18fe6
Julian Matschinske <julian.matschinske@bitspark.de>
2018-01-04 17:01:21 +0100

    Implement test method and add some example tests

--- tests/test_test.go
+++ tests/test_test.go
@@ -4,21 +4,29 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"slang"
-	"os"
+	"io/ioutil"
 )
 
 func TestTestOperator__TrivialTests(t *testing.T) {
 	a := assert.New(t)
-	fails, succs, err := slang.TestOperator("test_data/voidOp_test.json", os.Stdout)
+	succs, fails, err := slang.TestOperator("test_data/voidOp_test.json", ioutil.Discard, true)
 	a.Nil(err)
-	a.Equal(succs, 1)
-	a.Equal(fails, 0)
+	a.Equal(1, succs)
+	a.Equal(0, fails)
 }
 
 func TestTestOperator__SimpleFail(t *testing.T) {
 	a := assert.New(t)
-	fails, succs, err := slang.TestOperator("test_data/voidOp_corruptTest.json", os.Stdout)
+	succs, fails, err := slang.TestOperator("test_data/voidOp_corruptTest.json", ioutil.Discard, true)
 	a.Nil(err)
-	a.Equal(succs, 0)
-	a.Equal(fails, 1)
+	a.Equal(0, succs)
+	a.Equal(1, fails)
+}
+
+func TestTestOperator__ComplexTests(t *testing.T) {
+	a := assert.New(t)
+	succs, fails, err := slang.TestOperator("test_data/nested_op/usingSubCustomOpDouble_test.json", ioutil.Discard, true)
+	a.Nil(err)
+	a.Equal(2, succs)
+	a.Equal(0, fails)
 }

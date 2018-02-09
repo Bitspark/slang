5d1909bc5cfec27d5a47a4877ea996c1b255f729
Julian Matschinske <julian.matschinske@bitspark.de>
2018-02-09 19:29:42 +0100

    Add more tests for builtin.Const

--- builtin/const_test.go
+++ builtin/const_test.go
@@ -7,14 +7,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestOperatorCreator__Const__IsRegistered(t *testing.T) {
+func TestOperatorConst__IsRegistered(t *testing.T) {
 	a := assertions.New(t)
 
 	ocConst := getBuiltinCfg("const")
 	a.NotNil(ocConst)
 }
 
-func TestOperatorCreator__Const__RequireValue(t *testing.T) {
+func TestOperatorConst__NoProps(t *testing.T) {
 	a := assertions.New(t)
 	co, err := MakeOperator(
 		core.InstanceDef{
@@ -30,7 +30,142 @@ func TestOperatorCreator__Const__RequireValue(t *testing.T) {
 	a.Nil(co)
 }
 
-func TestOperatorCreator__Const__SimpleNumber(t *testing.T) {
+func TestBuiltinConst__NoGenerics(t *testing.T) {
+	a := assertions.New(t)
+	ao, err := MakeOperator(
+		core.InstanceDef{
+			Operator:   "const",
+			Properties: core.Properties{"value": 1.0},
+		},
+	)
+	a.Error(err)
+	a.Nil(ao)
+}
+
+func TestBuiltinConst__Correct(t *testing.T) {
+	a := assertions.New(t)
+	ao, err := MakeOperator(
+		core.InstanceDef{
+			Operator: "const",
+			Generics: map[string]*core.PortDef{
+				"valueType": {
+					Type: "number",
+				},
+			},
+			Properties: core.Properties{"value": 1.0},
+		},
+	)
+	require.NoError(t, err)
+	a.NotNil(ao)
+
+	a.Equal(core.TYPE_TRIGGER, ao.In().Type())
+	a.Equal(core.TYPE_NUMBER, ao.Out().Type())
+}
+
+func TestBuiltinConst__PushBoolean(t *testing.T) {
+	a := assertions.New(t)
+	ao, err := MakeOperator(
+		core.InstanceDef{
+			Operator: "const",
+			Generics: map[string]*core.PortDef{
+				"valueType": {
+					Type: "boolean",
+				},
+			},
+			Properties: core.Properties{"value": true},
+		},
+	)
+	require.NoError(t, err)
+	a.NotNil(ao)
+
+	a.Equal(core.TYPE_BOOLEAN, ao.Out().Type())
+
+	ao.Out().Bufferize()
+	ao.Start()
+
+	for i := 0; i < 20; i++ {
+		ao.In().Push(1)
+	}
+
+	a.PortPushes([]interface{}{true, true, true, true, true, true, true, true, true, true}, ao.Out())
+	a.PortPushes([]interface{}{true, true, true, true, true, true, true, true, true, true}, ao.Out())
+	// ...
+}
+
+func TestBuiltinConst__PushStream(t *testing.T) {
+	a := assertions.New(t)
+	ao, err := MakeOperator(
+		core.InstanceDef{
+			Operator: "const",
+			Generics: map[string]*core.PortDef{
+				"valueType": {
+					Type: "stream",
+					Stream: &core.PortDef{
+						Type: "primitive",
+					},
+				},
+			},
+			Properties: core.Properties{"value": []interface{}{1.0, "slang", true}},
+		},
+	)
+	require.NoError(t, err)
+	a.NotNil(ao)
+
+	a.Equal(core.TYPE_STREAM, ao.Out().Type())
+
+	ao.Out().Bufferize()
+	ao.Start()
+
+	for i := 0; i < 3; i++ {
+		ao.In().Push(1)
+	}
+
+	a.PortPushes([]interface{}{[]interface{}{1.0, "slang", true}}, ao.Out())
+	a.PortPushes([]interface{}{[]interface{}{1.0, "slang", true}}, ao.Out())
+	a.PortPushes([]interface{}{[]interface{}{1.0, "slang", true}}, ao.Out())
+	// ...
+}
+
+func TestBuiltinConst__PushMap(t *testing.T) {
+	a := assertions.New(t)
+	ao, err := MakeOperator(
+		core.InstanceDef{
+			Operator: "const",
+			Generics: map[string]*core.PortDef{
+				"valueType": {
+					Type: "map",
+					Map: map[string]*core.PortDef{
+						"a": {
+							Type: "number",
+						},
+						"b": {
+							Type: "primitive",
+						},
+					},
+				},
+			},
+			Properties: core.Properties{"value": map[string]interface{}{"a": 1.0, "b": false}},
+		},
+	)
+	require.NoError(t, err)
+	a.NotNil(ao)
+
+	a.Equal(core.TYPE_MAP, ao.Out().Type())
+
+	ao.Out().Bufferize()
+	ao.Start()
+
+	for i := 0; i < 3; i++ {
+		ao.In().Push(1)
+	}
+
+	a.PortPushes([]interface{}{map[string]interface{}{"a": 1.0, "b": false}}, ao.Out())
+	a.PortPushes([]interface{}{map[string]interface{}{"a": 1.0, "b": false}}, ao.Out())
+	a.PortPushes([]interface{}{map[string]interface{}{"a": 1.0, "b": false}}, ao.Out())
+	// ...
+}
+
+func TestOperatorConst__SimpleNumber(t *testing.T) {
 	a := assertions.New(t)
 	co, err := MakeOperator(
 		core.InstanceDef{
@@ -55,7 +190,7 @@ func TestOperatorCreator__Const__SimpleNumber(t *testing.T) {
 	a.PortPushes([]interface{}{5.0}, co.Out())
 }
 
-func TestOperatorCreator__Const__ComplexStreamMap(t *testing.T) {
+func TestOperatorConst__ComplexStreamMap(t *testing.T) {
 	a := assertions.New(t)
 	co, err := MakeOperator(
 		core.InstanceDef{
@@ -93,7 +228,7 @@ func TestOperatorCreator__Const__ComplexStreamMap(t *testing.T) {
 	a.PortPushes([]interface{}{"test"}, co.Out().Map("b"))
 }
 
-func TestOperatorCreator__Const__PassMarkers(t *testing.T) {
+func TestOperatorConst__PassMarkers(t *testing.T) {
 	a := assertions.New(t)
 	co, err := MakeOperator(
 		core.InstanceDef{

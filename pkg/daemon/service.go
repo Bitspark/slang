68c01391cb2cb36afef9ca6de4441f1af67bb7cb
Taleh Didover <taleh.didover@bitspark.de>
2018-05-06 19:58:20 +0200

    Allow passing cwd by get params + Fix list operators

--- pkg/daemon/service.go
+++ pkg/daemon/service.go
@@ -27,17 +27,10 @@ func writeJSON(w io.Writer, dat interface{}) error {
 	return json.NewEncoder(w).Encode(dat)
 }
 
-type inJSON struct {
-	WorkingDir       string                   `json:"workingdir"`
-	OperatorFilePath string                   `json:"operator"`
-	Generics         map[string]*core.TypeDef `json:"generics"`
-	Properties       *core.Properties         `json:"properties"`
-}
-
 type outJSON struct {
 	Objects []OperatorDefJSON `json:"objects"`
 	Status  string            `json:"status"`
-	Error   Error             `json:"error,omitempty"`
+	Error   *Error            `json:"error,omitempty"`
 }
 
 type OperatorDefJSON struct {
@@ -76,62 +69,66 @@ var OperatorDefService = &DaemonService{map[string]*DaemonEndpoint{
 
 	 */
 	"/": {func(w http.ResponseWriter, r *http.Request) {
-		var dataIn inJSON
 		var dataOut outJSON
+		var opDefList []OperatorDefJSON
 		var err error
-		readJSON(r.Body, &dataIn)
+		cwd := r.FormValue("cwd")
 
-		e := api.NewEnviron(dataIn.WorkingDir)
+		e := api.NewEnviron(cwd)
 
 		opNames, err := e.ListOperatorNames()
-		builtinOpNames := builtin.GetBuiltinNames()
-		opDefList := make([]OperatorDefJSON, len(opNames)+len(builtinOpNames))
 
-		offset := 0
 		if err == nil {
+			builtinOpNames := builtin.GetBuiltinNames()
+			opDefList = make([]OperatorDefJSON, len(opNames)+len(builtinOpNames))
+
 			// Gather builtin/elementary opDefs
+			offset := 0
 			for i, opFQName := range builtinOpNames {
-				if opDef, err := builtin.GetOperatorDef(opFQName); err != nil {
-					opDefList[offset+i] = OperatorDefJSON{
-						Name: opFQName,
-						Type: "elementary",
-						Def:  opDef,
-					}
-				}
-			}
+				opDef, err := builtin.GetOperatorDef(opFQName)
 
-			offset = len(builtinOpNames)
-
-			// Gather opDefs from local & lib
-			for i, opFQName := range opNames {
-
-				opDefFilePath, err := e.GetOperatorDefFilePath(strings.Replace(opFQName, ".", ".", -1), "")
-
-				if err == nil {
+				if err != nil {
 					break;
 				}
 
-				if opDef, err := e.ReadOperatorDef(opDefFilePath, nil); err != nil {
-
-					opType := "lib"
-					if e.IsLocalOperator(opDefFilePath) {
-						opType = "local"
-					}
-
-					opDefList[offset+i] = OperatorDefJSON{
-						Name: opFQName,
-						Type: opType,
-						Def:  opDef,
-					}
+				opDefList[offset+i] = OperatorDefJSON{
+					Name: opFQName,
+					Type: "elementary",
+					Def:  opDef,
 				}
 			}
 
+			if err == nil {
+				// Gather opDefs from local & lib
+				offset = len(builtinOpNames)
+				for i, opFQName := range opNames {
+					opDefFilePath, err := e.GetOperatorDefFilePath(strings.Replace(opFQName, ".", "/", -1), "")
+
+					if err != nil {
+						break;
+					}
+
+					if opDef, err := e.ReadOperatorDef(opDefFilePath, nil); err != nil {
+
+						opType := "lib"
+						if e.IsLocalOperator(opDefFilePath) {
+							opType = "local"
+						}
+
+						opDefList[offset+i] = OperatorDefJSON{
+							Name: opFQName,
+							Type: opType,
+							Def:  opDef,
+						}
+					}
+				}
+			}
 		}
 
-		if err != nil {
-			dataOut = outJSON{Status: "error", Error: Error{err.Error(), "E0001"}}
-		} else {
+		if err == nil {
 			dataOut = outJSON{Status: "success", Objects: opDefList}
+		} else {
+			dataOut = outJSON{Status: "error", Error: &Error{err.Error(), "E0001"}}
 		}
 
 		writeJSON(w, dataOut)

89a18c92fe2e937efefe6f0ff0204b6008a816f8
Julian Matschinske <julian.matschinske@bitspark.de>
2018-06-26 19:31:16 +0200

    Implement runner service packing HTTP handling operators

---
+++ pkg/api/run_http.go
@@ -0,0 +1,168 @@
+package api
+
+import "github.com/Bitspark/slang/pkg/core"
+
+// This file contains functions and structs needed for constructing operator execution surroundings for http endpoints
+
+// HttpRequest Slang type
+var HTTP_REQUEST_DEF = core.TypeDef{
+	Type: "map",
+	Map: map[string]*core.TypeDef{
+		"protocol": {
+			Type: "string",
+		},
+		"method": {
+			Type: "string",
+		},
+		"path": {
+			Type: "string",
+		},
+		"params": {
+			Type: "stream",
+			Stream: &core.TypeDef{
+				Type: "map",
+				Map: map[string]*core.TypeDef{
+					"key": {
+						Type: "string",
+					},
+					"values": {
+						Type: "stream",
+						Stream: &core.TypeDef{
+							Type: "string",
+						},
+					},
+				},
+			},
+		},
+		"headers": {
+			Type: "stream",
+			Stream: &core.TypeDef{
+				Type: "map",
+				Map: map[string]*core.TypeDef{
+					"key": {
+						Type: "string",
+					},
+					"value": {
+						Type: "string",
+					},
+				},
+			},
+		},
+		"body": {
+			Type: "binary",
+		},
+	},
+}
+
+// HttpResponse Slang type
+var HTTP_RESPONSE_DEF = core.TypeDef{
+	Type: "map",
+	Map: map[string]*core.TypeDef{
+		"status": {
+			Type: "number",
+		},
+		"headers": {
+			Type: "stream",
+			Stream: &core.TypeDef{
+				Type: "map",
+				Map: map[string]*core.TypeDef{
+					"key": {
+						Type: "string",
+					},
+					"value": {
+						Type: "string",
+					},
+				},
+			},
+		},
+		"body": {
+			Type: "binary",
+		},
+	},
+}
+
+// Constructs an executable operator
+// TODO: Make safer (maybe require an API key?)
+func ConstructHttpEndpoint(env *Environ, port int, operator string, gens core.Generics, props core.Properties) (*core.OperatorDef, error) {
+	httpDef := &core.OperatorDef{
+		ServiceDefs: map[string]*core.ServiceDef{
+			core.MAIN_SERVICE: {
+				In: core.TypeDef{
+					Type: "trigger",
+				},
+				Out: core.TypeDef{
+					Type: "trigger",
+				},
+			},
+		},
+		Connections: make(map[string][]string),
+	}
+
+	path, err := env.GetOperatorPath(operator, "")
+	if err != nil {
+		return nil, err
+	}
+
+	// Build operator to get interface and see if it is free from errors
+	// It will be compiled a second time later
+	op, err := env.BuildAndCompileOperator(path, gens, props)
+	if err != nil {
+		return nil, err
+	}
+
+	// Const port instance
+	portIns := &core.InstanceDef{
+		Name: "port",
+		Operator: "slang.const",
+		Generics: core.Generics{
+			"valueType": {
+				Type: "number",
+			},
+		},
+		Properties: core.Properties{
+			"value": float64(port),
+		},
+	}
+	httpDef.InstanceDefs = append(httpDef.InstanceDefs, portIns)
+	httpDef.Connections["("] = []string{"(port"}
+
+	// HTTP operator instance
+	httpIns := &core.InstanceDef{
+		Name: "httpServer",
+		Operator: "slang.net.httpServer",
+	}
+	httpDef.InstanceDefs = append(httpDef.InstanceDefs, httpIns)
+	httpDef.Connections["port)"] = []string{"(httpServer"}
+	httpDef.Connections["httpServer)"] = []string{")"}
+
+	// The HTTP server is connected now, only the handler delegate is missing
+
+	// This is the actual operator we want to execute
+	operatorIns := &core.InstanceDef{
+		Name: "operator",
+		Operator: operator,
+		Generics: gens,
+		Properties: props,
+	}
+	httpDef.InstanceDefs = append(httpDef.InstanceDefs, operatorIns)
+
+	// Get operator interface
+	inDef := op.Main().In().Define()
+	outDef := op.Main().Out().Define()
+
+	if inDef.Equals(HTTP_REQUEST_DEF) {
+		// If the operator can handle HTTP requests itself, just pass them
+		httpDef.Connections["httpServer.handler)~"] = []string{"(operator"}
+	} else {
+		panic("not implemented")
+	}
+
+	if outDef.Equals(HTTP_RESPONSE_DEF) {
+		// If the operator produces HTTP responses itself, just pass them
+		httpDef.Connections["operator)"] = []string{"~(httpServer.handler"}
+	} else {
+		panic("not implemented")
+	}
+
+	return httpDef, nil
+}
--- pkg/daemon/runner_service.go
+++ pkg/daemon/runner_service.go
@@ -1,8 +1,55 @@
 package daemon
 
-import "net/http"
+import (
+	"net/http"
+	"encoding/json"
+	"github.com/Bitspark/slang/pkg/api"
+	"github.com/Bitspark/slang/pkg/core"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"gopkg.in/yaml.v2"
+)
 
 var RunnerService = &DaemonService{map[string]*DaemonEndpoint{
 	"/": {func(w http.ResponseWriter, r *http.Request) {
+		type runInstructionJSON struct {
+			Cwd   string          `json:"cwd"`
+			Fqn   string          `json:"fqn"`
+			Props core.Properties `json:"props"`
+			Gens  core.Generics   `json:"gens"`
+		}
+
+		type outJSON struct {
+			URL    string `json:"url,omitempty"`
+			Status string `json:"status"`
+			Error  *Error `json:"error,omitempty"`
+		}
+
+		var data outJSON
+
+		decoder := json.NewDecoder(r.Body)
+		var ri runInstructionJSON
+		err := decoder.Decode(&ri)
+		if err != nil {
+			data = outJSON{Status: "error", Error: &Error{Msg: err.Error(), Code: "E0001"}}
+			writeJSON(w, &data)
+			return
+		}
+
+		env := api.NewEnviron(ri.Cwd)
+		httpDef, err := api.ConstructHttpEndpoint(env, 12345, ri.Fqn, ri.Gens, ri.Props)
+		if err != nil {
+			data = outJSON{Status: "error", Error: &Error{Msg: err.Error(), Code: "E0002"}}
+			writeJSON(w, &data)
+			return
+		}
+
+		bytes, _ := yaml.Marshal(httpDef)
+		ioutil.WriteFile(
+			filepath.Join(env.WorkingDir(), strings.Replace(ri.Fqn + "Packed", ".", string(filepath.Separator), -1) + ".yaml"),
+			bytes,
+			0644,
+		)
 	}},
 }}

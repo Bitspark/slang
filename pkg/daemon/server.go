a5a140b501f36413bbb5e2f31fcab0f1b18fdef5
Kai Richard Koenig <kai.koenig@paessler.com>
2019-05-03 10:31:53 +0200

    Add the most basic implementation of websockets.
    If connected the socket just sends "hello" every seconds and prints
    every message recieved on the socket.

--- pkg/daemon/server.go
+++ pkg/daemon/server.go
@@ -1,18 +1,43 @@
 package daemon
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/Bitspark/slang/pkg/env"
 
 	"github.com/rs/cors"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 var SlangVersion string
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// allow every host/origin to make a connection e.g. :8080 -> 5149
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
 
 type Server struct {
 	Host   string
@@ -35,11 +60,71 @@ func NewServer(ctx *context.Context, env *env.Environment) *Server {
 	return srv
 }
 
+func (s *Server) AddWebsocket(path string) {
+	r := s.router.Path(path)
+	r.HandlerFunc(serveWs)
+}
+
+func reader(ws *websocket.Conn) {
+	newline := []byte{'\n'}
+	space := []byte{' '}
+	defer ws.Close()
+	ws.SetReadLimit(maxMessageSize)
+	ws.SetReadDeadline(time.Now().Add(pongWait))
+	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+
+	for {
+		_, message, err := ws.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		log.Printf("Got Message: %v", message)
+	}
+}
+
+func writer(ws *websocket.Conn) {
+	ticker := time.NewTicker(pingPeriod)
+	helloTicker := time.NewTicker(1 * time.Second)
+	defer func() {
+		ticker.Stop()
+		ws.Close()
+	}()
+	for {
+		select {
+		case <-helloTicker.C:
+			ws.SetWriteDeadline(time.Now().Add(writeWait))
+			ws.WriteMessage(websocket.TextMessage, []byte("Hello"))
+		case <-ticker.C:
+			ws.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := ws.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		if _, ok := err.(websocket.HandshakeError); !ok {
+			log.Println(err)
+		}
+		return
+	}
+	go writer(ws)
+	go reader(ws)
+}
+
 func (s *Server) mountWebServices() {
 	s.AddService("/operator", DefinitionService)
 	s.AddService("/run", RunnerService)
 	s.AddService("/share", SharingService)
 	s.AddOperatorProxy("/instance")
+	s.AddWebsocket("/ws")
 }
 
 func (s *Server) AddService(pathPrefix string, services *Service) {

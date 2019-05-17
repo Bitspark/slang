4b4606420ff102ade5a420dd3c61cc8bb15b1277
Taleh Didover <taleh.didover@bitspark.de>
2019-05-17 18:04:39 +0200

    Rename Port.StringifyComplete into Port.String

--- pkg/core/port.go
+++ pkg/core/port.go
@@ -193,7 +193,7 @@ func (p *Port) Connect(q *Port) (err error) {
 		if q.src == p {
 			return nil
 		}
-		return fmt.Errorf("%s -> %s: already connected", p.StringifyComplete(), q.StringifyComplete())
+		return fmt.Errorf("%s -> %s: already connected", p.String(), q.String())
 	}
 
 	if q.itemType == TYPE_PRIMITIVE {
@@ -652,7 +652,7 @@ func (p *Port) PullBinary() (Binary, interface{}) {
 func (p *Port) PullBOS() bool {
 	i := p.sub.Pull()
 	if !p.OwnBOS(i) {
-		panic(p.operator.Name() + ": expected own BOS: " + i.(BOS).src.StringifyComplete() + " != " + p.strSrc.StringifyComplete())
+		panic(p.operator.Name() + ": expected own BOS: " + i.(BOS).src.String() + " != " + p.strSrc.String())
 	}
 	return true
 }
@@ -723,7 +723,7 @@ func (p *Port) Name() string {
 		return "<nil>"
 	}
 
-	return p.StringifyComplete()
+	return p.String()
 }
 
 func (p *Port) Bufferize() {
@@ -907,7 +907,7 @@ func (p *Port) stringify() string {
 	return ""
 }
 
-func (p *Port) StringifyComplete() string {
+func (p *Port) String() string {
 	opStr := ""
 	if p.operator != nil {
 		opStr = p.operator.name
@@ -932,12 +932,12 @@ func (p *Port) StringifyComplete() string {
 }
 
 func (p *Port) defineConnections(def *OperatorDef) {
-	portStr := p.StringifyComplete()
+	portStr := p.String()
 
 	if def.Connections[portStr] == nil {
 		def.Connections[portStr] = make([]string, 0)
 		for dst := range p.dests {
-			def.Connections[portStr] = append(def.Connections[portStr], dst.StringifyComplete())
+			def.Connections[portStr] = append(def.Connections[portStr], dst.String())
 			dst.operator.defineConnections(def)
 		}
 	}

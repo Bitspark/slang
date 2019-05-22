9e9198d9fc60f076cd562fd32a7d568caded8ff9
Kai Richard Koenig <kai.koenig@paessler.com>
2019-05-22 08:18:31 +0200

    Debounce and collected message to be sent later

--- pkg/daemon/server.go
+++ pkg/daemon/server.go
@@ -70,7 +70,7 @@ type Hub struct {
 // Receiving end must of course know that message can hold 1+N message and dispatch accordingly.
 type envelop struct {
 	receiver *UserID
-	messages []message
+	messages []*message
 }
 
 // A message is holding data and a topic - we do this so the interface can listen on different topics
@@ -151,42 +151,94 @@ func (e *envelop) Bytes() []byte {
 	return body
 }
 
+// An Envelop is able to hold an unlimited amount of messages intended for one
+// receiver e.g. UserID, this is a variadic method that allows appending *message(s).
+func (e *envelop) append(m ...*message) {
+	e.messages = append(e.messages, m...)
+}
+
 // Send a message to single user on all his connections
 // This API is probably a little volatile so use with caution and don't reach deep into it.
 func (h *Hub) broadCastTo(u *UserID, topic Topic, data interface{}) {
-	var messages []message
-	messages = append(messages, message{topic, data})
+	var messages []*message
+	messages = append(messages, &message{topic, data})
 	h.broadcast <- &envelop{u, messages}
 }
 
 func (h *Hub) run() {
+	var (
+		incomingEnvelop *envelop
+		ok              bool
+		minTimer        <-chan time.Time
+		maxTimer        <-chan time.Time
+	)
+	min := 100 * time.Millisecond
+	max := 500 * time.Millisecond
+	postBox := make(map[*UserID]*envelop)
+
+	actualSend := func(letter *envelop) {
+		for client := range h.clients {
+			// this might become PINA as iterating all clients to find only those which we want to address
+			// could get expensive - maybe look up the clients by `userID` in the first place.
+			if client.userID != letter.receiver {
+				continue
+			}
+			// wrapping `<-` with a `select` and `default` makes it non-blocking if there is no reciever on the other reading off the channel.
+			// The channel is buffered meaning that we can sucessfully write into it as long as a reciever is pulling data from the other end.
+			select {
+			case client.send <- letter.Bytes():
+				// message written
+			default:
+				// buffer of channel full - no one reading? Let's disconnect them.
+				close(client.send)
+				delete(h.clients, client)
+			}
+		}
+	}
+
 	for {
 		select {
 		case client := <-h.register:
 			h.clients[client] = true
 		case client := <-h.unregister:
-			if _, ok := h.clients[client]; ok {
+			if _, ok = h.clients[client]; ok {
 				delete(h.clients, client)
 				close(client.send)
 			}
-		case letter := <-h.broadcast:
-			for client := range h.clients {
-				// this might become PINA as iterating all clients to find only those which we want to address
-				// could get expensive
-				if client.userID != letter.receiver {
-					continue
-				}
-				// wrapping `<-` with a `select` and `default` makes it non-blocking if there is no reciever on the other reading off the channel.
-				// The channel is buffered meaning that we can sucessfully write into it as long as a reciever is pulling data from the other end.
-				select {
-				case client.send <- letter.Bytes():
-					// message written
-				default:
-					// buffer of channel full - no one reading? Let's disconnect them.
-					close(client.send)
-					delete(h.clients, client)
-				}
+		case incomingEnvelop = <-h.broadcast:
+			// Keep adding messages from other envelop for at least `min` amount of time to the postbox.
+			minTimer = time.After(min)
+			// We only want to set the how long we are going to keep adding messages once.
+			if maxTimer == nil {
+				maxTimer = time.After(max)
 			}
+			// If we already have a envelop for that user, appened the contents of the current envelop
+			// to the one that is already scheduled to be sent.
+			if _, ok := postBox[incomingEnvelop.receiver]; ok {
+				// What we do here is to grow the single envelop`s messages.
+				postBox[incomingEnvelop.receiver].append(incomingEnvelop.messages...)
+			} else {
+				// If do not yet have a letter to be sent create one for the current receiver.
+				postBox[incomingEnvelop.receiver] = incomingEnvelop
+			}
+
+		case <-minTimer:
+			// We have not seen new letters since `min` time which means we can now send
+			// send the letters to all users.
+			minTimer, maxTimer = nil, nil
+			for _, e := range postBox {
+				actualSend(e)
+			}
+			// we also need to clear reset our letters since we have sent them all
+			postBox = make(map[*UserID]*envelop)
+		case <-maxTimer:
+			// Enough time has now passed and we should now send what we have already collected.
+			// This useful in scenarios where we send just enough data to
+			minTimer, maxTimer = nil, nil
+			for _, e := range postBox {
+				actualSend(e)
+			}
+			postBox = make(map[*UserID]*envelop)
 		}
 	}
 }

8756806310898e5d03196aaafff04a40816d6a2d
Taleh Didover <taleh.didover@bitspark.de>
2019-06-14 19:33:55 +0200

    Add deploy instance api and test

---
+++ cmd/deployment/handlers.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/Bitspark/slang/pkg/core"
+)
+
+func responseWithError(w http.ResponseWriter, err error, status int) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(err); err != nil {
+		panic(err)
+	}
+}
+
+func responseWithOk(w http.ResponseWriter, m interface{}) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(m); err != nil {
+		panic(err)
+	}
+}
+
+func getBodyBufferSafe(r *http.Request) []byte {
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 10485760)) // limit to 10 MB
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+
+	return body
+}
+
+type DeploymentMode struct {
+	Mode        T_DeploymentMode
+	Title       string
+	Description string
+}
+
+type DeploymentModes []DeploymentMode
+
+func listDeploymentModes(w http.ResponseWriter, r *http.Request) {
+	m := DeploymentModes{
+		{Mode: Rest, Title: "REST", Description: "REST"},
+		{Mode: Form, Title: "FORM", Description: "FORM"},
+		{Mode: Process, Title: "PROCESS", Description: "PROCESS"},
+	}
+
+	responseWithOk(w, m)
+}
+
+type DeployInstruction struct {
+	SlangFile core.SlangFileDef
+	Mode      T_DeploymentMode
+}
+
+func deployInstance(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	body := getBodyBufferSafe(r)
+
+	var instruction DeployInstruction
+	if err := json.Unmarshal(body, &instruction); err != nil {
+		responseWithError(w, err, http.StatusBadRequest)
+		return
+	}
+
+	deployer := getDeployer(r)
+
+	instDef, err := deployer.Deploy(instruction.SlangFile, instruction.Mode)
+
+	if err != nil {
+		responseWithError(w, err, http.StatusBadRequest)
+		return
+	}
+
+	responseWithOk(w, instDef)
+}
--- cmd/deployment/main.go
+++ cmd/deployment/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
-	"io"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/Bitspark/slang/pkg/core"
@@ -95,12 +92,16 @@ func addContext(ctx context.Context, next http.Handler) http.Handler {
 	})
 }
 
+func getDeployer(r *http.Request) Deployer {
+	return r.Context().Value("deployer").(Deployer)
+}
+
 func newRouter(deployer Deployer) http.Handler {
 	r := mux.NewRouter()
 
 	registerHandler(r)
 
-	ctx := context.Background()
+	ctx := context.WithValue(context.Background(), "deployer", deployer)
 
 	return addContext(ctx, r)
 }
@@ -108,57 +109,4 @@ func newRouter(deployer Deployer) http.Handler {
 func registerHandler(r *mux.Router) {
 	r.PathPrefix("/api/v1/modes").Methods("GET").HandlerFunc(listDeploymentModes)
 	r.PathPrefix("/api/v1/instances").Methods("POST").HandlerFunc(deployInstance)
-}
-
-// --- Handler
-
-type DeploymentMode struct {
-	Mode        T_DeploymentMode
-	Title       string
-	Description string
-}
-
-type DeploymentModes []DeploymentMode
-
-func listDeploymentModes(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-
-	m := DeploymentModes{
-		{Mode: Rest, Title: "REST", Description: "REST"},
-		{Mode: Form, Title: "FORM", Description: "FORM"},
-		{Mode: Process, Title: "PROCESS", Description: "PROCESS"},
-	}
-
-	if err := json.NewEncoder(w).Encode(m); err != nil {
-		panic(err)
-	}
-}
-
-type DeployInstruction struct {
-	slangFile core.SlangFileDef
-	mode      T_DeploymentMode
-}
-
-func deployInstance(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 10485760)) // limit to 10 MB
-	if err != nil {
-		panic(err)
-	}
-	if err := r.Body.Close(); err != nil {
-		panic(err)
-	}
-
-	var instruction DeployInstruction
-	if err := json.Unmarshal(body, &instruction); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		if err := json.NewEncoder(w).Encode(err); err != nil {
-			panic(err)
-		}
-	}
-
-	w.WriteHeader(http.StatusOK)
-	// ...
 }

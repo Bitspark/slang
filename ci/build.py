35cd44e50fbbddfa0ad28c7f323c84cd6f67da38
Taleh Didover <taleh.didover@bitspark.de>
2019-07-18 10:55:50 +0200

    Build slang runner on release

--- ci/build.py
+++ ci/build.py
@@ -1,18 +1,14 @@
 import sys
 import time
-from os import system, chdir
+from os import chdir
+
 from utils import execute_commands
 
 OS = ['darwin', 'linux', 'windows']
 ARCHS = ['386', 'amd64']
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Usage: python3 build.py vx.y.z [b6k_cs_pw]')
-        exit(-1)
 
-    version = sys.argv[1]
-    b6k_cs_pw = sys.argv[2] if len(sys.argv) > 2 else None
+def build_slangd(version, b6k_cs_pw):
     versioned_dist = 'slangd-' + version.replace('.', '_')
     build_time = int(time.time())
 
@@ -47,3 +43,39 @@ if __name__ == '__main__':
                 f"rm {filename_with_ending}",
             ])
             chdir("../..")
+
+
+def build_slang(version):
+    versioned_dist = 'slang-' + version.replace('.', '_')
+
+    for os in OS:
+        for arch in ARCHS:
+            filename_with_ending = filename = f"{versioned_dist}-{os}-{arch}"
+            if os == 'windows':
+                filename_with_ending += ".exe"
+                compress_cmd = f"zip {filename}.zip {filename_with_ending}"
+            else:
+                compress_cmd = f"tar -czvf {filename}.tar.gz {filename_with_ending}"
+
+            execute_commands([
+                f"env GOOS={os} GOARCH={arch} go build -o ./ci/release/{filename_with_ending} ./cmd/slang",
+            ])
+
+            chdir("./ci/release/")
+            execute_commands([
+                compress_cmd,
+                f"rm {filename_with_ending}",
+            ])
+            chdir("../..")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: python3 build.py vx.y.z [b6k_cs_pw]')
+        exit(-1)
+
+    version = sys.argv[1]
+    b6k_cs_pw = sys.argv[2] if len(sys.argv) > 2 else None
+
+    build_slangd(version, b6k_cs_pw)
+    build_slang(version)

cb67e887d50d4ede77662c300fc9423fb9f6752a
Taleh Didover <dydooov+github@gmail.com>
2022-11-23 12:33:53 +0100

    Unify response data structure

--- pkg/daemon/run_operator_service.go
+++ pkg/daemon/run_operator_service.go
@@ -13,24 +13,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type outJSON struct {
-	Objects []*runningOperator `json:"objects"`
-	Status  string             `json:"status"`
-	Error   *Error             `json:"error,omitempty"`
-}
-
 type RunInstructionJSON struct {
 	Blueprint uuid.UUID       `json:"blueprint"`
 	Props     core.Properties `json:"props"`
 	Gens      core.Generics   `json:"gens"`
 	Stream    bool            `json:"stream"`
-}
-
-type RunInstructionResponseJSON struct {
-	Handle string `json:"handle,omitempty"`
-	URL    string `json:"url,omitempty"`
-	Status string `json:"status"`
-	Error  *Error `json:"error,omitempty"`
 }
 
 var RunOperatorService = &Service{map[string]*Endpoint{
@@ -40,30 +27,44 @@ var RunOperatorService = &Service{map[string]*Endpoint{
 			/*
 				Get all running operators
 			*/
-			resp := outJSON{Objects: funk.Values(runningOperatorManager.ops).([]*runningOperator), Status: "success", Error: nil}
+			type responseListJSON struct {
+				Objects []*runningOperator `json:"objects"`
+				Status  string             `json:"status"`
+				Error   *Error             `json:"error,omitempty"`
+			}
+
+			resp := responseListJSON{Objects: funk.Values(runningOperatorManager.ops).([]*runningOperator), Status: "success", Error: nil}
 			writeJSON(w, &resp)
 
 		} else if r.Method == "POST" {
 			/*
 				Start operator
 			*/
+			type responseSingleJSON struct {
+				Object *runningOperator `json:"object"`
+				Status string           `json:"status"`
+				Error  *Error           `json:"error,omitempty"`
+			}
+
 			hub := GetHub(r)
 			st := GetStorage(r)
 
 			var requ RunInstructionJSON
-			var resp RunInstructionResponseJSON
+			var resp responseSingleJSON
 
 			decoder := json.NewDecoder(r.Body)
 			err := decoder.Decode(&requ)
 			if err != nil {
-				resp = RunInstructionResponseJSON{Status: "error", Error: &Error{Msg: err.Error(), Code: "E0001"}}
+				resp = responseSingleJSON{Status: "error", Error: &Error{Msg: err.Error(), Code: "E0001"}}
+				w.WriteHeader(400)
 				writeJSON(w, &resp)
 				return
 			}
 
 			op, err := api.BuildAndCompile(requ.Blueprint, requ.Gens, requ.Props, st)
 			if err != nil {
-				resp = RunInstructionResponseJSON{Status: "error", Error: &Error{Msg: err.Error(), Code: "E0002"}}
+				resp = responseSingleJSON{Status: "error", Error: &Error{Msg: err.Error(), Code: "E0002"}}
+				w.WriteHeader(400)
 				writeJSON(w, &resp)
 				return
 			}
@@ -89,8 +90,7 @@ var RunOperatorService = &Service{map[string]*Endpoint{
 			}()
 
 			resp.Status = "success"
-			resp.Handle = runOp.Handle
-			resp.URL = runOp.URL
+			resp = responseSingleJSON{Object: runOp, Status: "success"}
 
 			writeJSON(w, &resp)
 

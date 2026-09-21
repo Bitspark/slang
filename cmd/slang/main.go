40b29039ab1ac995dde3f7ab4f018c3e418c4fcd
Julian Matschinske <julian.matschinske@bitspark.com>
2026-09-21 19:33:02 +0200

    Reject unconnected blueprint outputs before running

--- cmd/slang/main.go
+++ cmd/slang/main.go
@@ -86,9 +86,16 @@ func readSlangBundleJSON(slBundlePath string) (*core.SlangBundle, error) {
 }
 
 func run(operator *core.Operator, mode string, bind string) error {
+	if operator.Main() == nil {
+		return errors.New("blueprint has no main service")
+	}
+	if err := operator.Main().Out().FullyConnected(); err != nil {
+		return err
+	}
 	// Handle SIGTERM (CTRL-C)
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	defer signal.Stop(quit)
 
 	switch mode {
 	case "process":
@@ -102,7 +109,7 @@ func run(operator *core.Operator, mode string, bind string) error {
 			quit <- syscall.SIGQUIT
 		}()
 	default:
-		log.Fatal("run mode not supported: %s", mode)
+		return fmt.Errorf("run mode not supported: %s", mode)
 	}
 
 	for {
